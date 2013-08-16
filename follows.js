a91afe530c737b5b0865e168426f87ba0b1fe94b
sanlevaca <sanlevaca@ovi.com>
2013-08-16 11:52:29 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,12 +13,12 @@
 	*/
 
 	var defaultKey		= 'vpm5Z9hBCzX9F55sYsqVOpCLHm9e5nkM8IdHON8yfPwO4mUH', // Unique master Xively API key to be used as a default
-		defaultFeeds	= ['1336995829 Temp0'], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= [1336995829], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'PCM Refrigerator', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
