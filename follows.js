7449adbdfe12e634fab6dbe351f8b86d0e6d7357
sanlevaca <sanlevaca@ovi.com>
2013-08-27 16:54:26 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= 'vpm5Z9hBCzX9F55sYsqVOpCLHm9e5nkM8IdHON8yfPwO4mUH', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1336995829], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'zmGUnZqWyDjqbiozPQq5LNjFBhfc9jBl0QOesdaYFsIjLUwY', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [358590698], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'PCM Refrigerator', // Replaces Xively logo in the header
 		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
